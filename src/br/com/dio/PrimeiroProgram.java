009e57483a0c08b4952b4029cada75f3adf57ac9
DanielBico <danielvendito@gmail.com>
2021-10-19 21:18:08 -0300

    criando lista livro

--- src/br/com/dio/PrimeiroProgram.java
+++ src/br/com/dio/PrimeiroProgram.java
@@ -7,8 +7,47 @@ public class PrimeiroProgram {
         Gato gato = new Gato();
         System.out.println(gato);
 
-        /*int a = 5;
+
+        Livro livro1 = new Livro("O problema dos 3 corpos", 300);
+        System.out.println(livro1);
+
+    /*    int a = 5;
                 int b = 3;
         System.out.println("Hello World! " + (a+b));*/
     }
 }
+
+class Livro {
+    private String nome;
+    private Integer numPage;
+
+    public Livro(String nome, Integer numPage) {
+        this.nome = nome;
+        this.numPage = numPage;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPage() {
+        return numPage;
+    }
+
+    public void setNumPage(Integer numPage) {
+        this.numPage = numPage;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPage=" + numPage +
+                '}';
+    }
+}
+
